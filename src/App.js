f99beeef17b50caf35d4a8036132423d6f6ca1e6
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-27 12:48:15 +0530

    Fetching Todos API !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const[todos, setTodos] = useState([])
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos').then((result)=>{
+      result.json().then((res)=>{
+        console.log(res)
+        setTodos(res)
+      })
+      })
+    }, [])
+console.log(todos);
+
   return (
     <div className="App">
         <h1>TODO LIST REACT</h1> 
