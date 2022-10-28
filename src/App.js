f617aedfa55a01d33f2fe947536f145f8abc4281
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-28 11:05:11 +0530

    Created Add Todo Functionality !

--- src/App.js
+++ src/App.js
@@ -2,24 +2,61 @@ import React, {useEffect, useState} from "react";
 
 function App() {
   const[todos, setTodos] = useState([])
+  const[title, setTitle] = useState('');
+  const[completed, setCompleted] = useState(false);
+
 
   useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos').then((result)=>{
+    getTodos()
+    }, [])
+
+    function getTodos(){
+      fetch('https://jsonplaceholder.typicode.com/todos').then((result)=>{
       result.json().then((res)=>{
         console.log(res)
         setTodos(res)
+        setTitle(res[0].title)
+        setCompleted(res[0].completed)
+
       })
       })
-    }, [])
-console.log(todos);
+    }
+
+    //ADDING A TODO
+
+    function addTodo(){
+      console.log({title, completed, });
+      let index = todos.length;
+      const data = {title, completed, id:index+1,userId:1}
+      fetch('https://jsonplaceholder.typicode.com/todos',{
+        method:'POST',
+        headers:{
+          'Accept':'application/json',
+          'Content-type':'application/json'
+        },
+        body:  JSON.stringify(data)
+      }).then(()=>{
+        setTodos([...todos, data ])
+      })
+    }
 
   return (
     <div className="App">
         <h1>TODO LIST REACT</h1>
+        <input type="text" value={title} onChange={(e) => {setTitle(e.target.value)}} name="name" /> 
+        <input type="text" value={completed} onChange={(e) => {setCompleted(e.target.value)}} name="completed" /> 
+        <button type="button" className="btn" onClick={addTodo}>Add</button>
         <hr />
         <div className="container">
         {
-          todos.map((todo)=> ( <div key={todo.id}  className="InnerDiv"><div className="num">{todo.id} </div> <div className="title">{todo.title}</div> <div className="status"> Status= <input type="checkbox" checked={todo.completed} /></div> </div> ))}
+          todos.map((todo)=> {
+           return <div key={todo.id-1}  className="InnerDiv">
+            <div className="title">{todo.title}</div> 
+            <div className="status"> Status= <input type="checkbox" checked={todo.completed} defaultChecked={todo.completed} /></div> 
+            <div> 
+              </div>
+             </div> 
+          })}
         </div>
     </div>
   );
